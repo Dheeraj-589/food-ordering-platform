75df0a65ad780e9f6976c7dd178f8c5d54c5ed17
dheeraj gakkampudi <dheerajdj589@gmail.com>
2026-07-08 01:02:32 +0530

    done sync

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -15,8 +15,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         password: configService.get<string>('database.password'),
         database: configService.get<string>('database.database'),
         autoLoadEntities: true,
-        synchronize: true,
-        logging: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'production', // Dev-only auto schema sync
+        logging: configService.get<string>('NODE_ENV') === 'development',
       }),
     }),
   ],
